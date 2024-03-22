7512ead1f088fd4835f6c5cd7c763e0d07cda31e
Your Name <your.email@example.com>
2024-03-22 15:51:32 +0800

    upload

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 /* Problem 1 Interface & Contract */
+
 contract StudentV1 {
     // Note: You can declare some state variable
+    uint256 public registerValue = 1001;
+    uint256 public value = 123;
+    bool public success = true;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+       if (success) {
+            success = false;
+            return registerValue;
+        } else {
+            return value; 
+    }
     }
 }
 
@@ -16,14 +25,29 @@ interface IClassroomV2 {
 }
 
 contract StudentV2 {
+    uint256 public registerValue = 1001;
+    uint256 public value = 123;
+
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
-    }
+        if(IClassroomV2(msg.sender).isEnrolled() == false){
+            return registerValue;
+        }
+        else{
+            return value;
+        }
+}
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+    // TODO: please add your implementaiton here
+        uint256 currentgas = gasleft();
+        if(currentgas > 6623 wei){
+            return currentgas;
+        }
+        else{
+            return 123;
+        }
     }
 }
